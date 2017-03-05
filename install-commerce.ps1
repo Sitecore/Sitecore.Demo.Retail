b02d13fbb0aba1f740b5527e6f2987251da3fdcd
Thomas Eldblom <the@sitecore.net>
2017-03-06 08:36:26 +1100

    Reserialized items based on changed templates Minor Commerce script fixes Added Commerce product databases to retail project

--- install-commerce.ps1
+++ install-commerce.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Unrestricted –scope CurrentUser
 if (-Not ($PSVersionTable.PSVersion.Major -ge 4)) { Write-Host "Update version of powershell"; exit }
 
 #If Install-Module is not available - install https://www.microsoft.com/en-us/download/details.aspx?id=51451
-if((Get-Module Carbon) -eq $null) { Install-Module -Name 'Carbon'; Import-Module 'Carbon' }
+if((Get-Module Carbon) -eq $null) { Install-Module -Name 'Carbon' -AllowClobber; Import-Module 'Carbon' }
 Import-Module $PSScriptRoot\Scripts\Commerce\ManageUser.psm1 -Force
 Import-Module $PSScriptRoot\Scripts\Commerce\ManageFile.psm1 -Force
 Import-Module $PSScriptRoot\Scripts\Commerce\ManageCommerceServer.psm1 -Force
@@ -14,7 +14,7 @@ Import-Module $PSScriptRoot\Scripts\Commerce\ManageSqlServer.psm1 -Force
 cd $PSScriptRoot
 
 If ((ManageUser\Confirm-Admin) -ne $true) { Write-Host "Please run script as administrator"; exit }
-If ((ManageRegistry\Get-InternetExplorerEnhancedSecurityEnabled) -eq $true) { Write-Host "Please disable Internet Explorer Enhanced Security"; exit }
+#If ((ManageRegistry\Get-InternetExplorerEnhancedSecurityEnabled) -eq $true) { Write-Host "Please disable Internet Explorer Enhanced Security"; exit }
 
 $settings = ((Get-Content $PSScriptRoot\install-commerce-config.json -Raw) | ConvertFrom-Json)
 $runTimeUserSetting = ($settings.accounts | Where { $_.id -eq "runTime" } | Select)
@@ -24,17 +24,16 @@ If ($installFolderSetting -eq $null) { Write-Host "Expected install resources" -
   
 $csResourceFolderSetting = ($settings.resources | Where { $_.id -eq "commerceServerResources" } | Select)
 If ($csResourceFolderSetting -eq $null) { Write-Host "Expected dacpac resources" -ForegroundColor red; exit; }         
-
 # Step 0: check if all files exist that are needed
 Write-Host "`nStep 0: Checking if all needed files exist" -foregroundcolor Yellow
 if ((ManageFile\Confirm-Resources $settings.resources -verbose) -ne 0) { Exit }
-<#
+
 # Step 1: Create Required Users
 Write-Host "`nStep 1: Create Required Users" -foregroundcolor Yellow
 If((ManageUser\Add-User -user $runTimeUserSetting -Verbose) -ne 0) { Exit }
 
 # Step 2: Create Database Logins
-Write-Host "`nStep 2: Create Databse Logins" -foregroundcolor Yellow
+Write-Host "`nStep 2: Create Database Logins" -foregroundcolor Yellow
 If((ManageSqlServer\New-SqlLogin -accountId "runTime" -databaseId "commerceAdminDB" -accountSettingList $settings.accounts -databaseSettingList $settings.databases -Verbose) -ne 0) { Exit }
 
 # Step 3: Run Commerce Server Installer
@@ -65,8 +64,8 @@ If((ManageCommerceServer\New-CSWebsite -csSiteSetting $settings.sitecoreCommerce
 
 # Step 9: Import Database Changes
 Write-Host "`nStep 9: Import Database Changes" -foregroundcolor Yellow
-If((ManageSqlServer\Import-DatabaseChanges -databaseSettingList $settings.databases -resourcesSettingList $settings.resources -resourceFolderId "projectDatabaseScripts"  -Verbose) -ne 0) { Exit }
 If((ManageSqlServer\Import-DatabaseChanges -databaseSettingList $settings.databases -resourcesSettingList $settings.resources -resourceFolderId "foundationDatabaseScripts"  -Verbose) -ne 0) { Exit }
+If((ManageSqlServer\Import-DatabaseChanges -databaseSettingList $settings.databases -resourcesSettingList $settings.resources -resourceFolderId "projectDatabaseScripts"  -Verbose) -ne 0) { Exit }
 
 # Step 10: Import Commerce Server Resources
 Write-Host "`nStep 10: Import Commerce Server Resources" -foregroundcolor Yellow
@@ -80,7 +79,6 @@ ManageRegistry\Disable-LoopbackCheck
 Write-Host "`nStep 12: Test Commerce Server WebServices" -foregroundcolor Yellow
 If((ManageCommerceServer\Test-CSWebservices -csSiteSetting $settings.sitecoreCommerce.csSite -accountSettingList $settings.accounts -appPoolSettingList $settings.iis.appPools -websiteSettingList $settings.iis.websites -Verbose) -ne 0) { Exit }
 
-
 # Step 13: Restore Solution Packages 
 Write-Host "`nStep 13: Restore Solution Packages" -foregroundcolor Yellow
 If((ManageDotNet\Restore-SolutionPackages -resourcesSettingList $settings.resources -resourceId "solutionRoot" -Verbose) -ne 0) { Exit }
@@ -93,12 +91,11 @@ If((ManageDotNet\Publish-Project -resourcesSettingList $settings.resources -webs
 # Step 15: Test Commerce Engine
 Write-Host "`nStep 15: Test Commerce Engine" -foregroundcolor Yellow
 If((ManageWeb\Invoke-WebRequest -websiteSettingList $settings.iis.websites -websiteId "commerceEngine" -relativeUri "api/`$metadata" -Verbose) -ne 0) { Exit }
-
 # Step 16: Bootstrap Commerce Engine and Initialise Environments
 Write-Host "`nStep 16: Test Commerce Engine" -foregroundcolor Yellow
 If((ManageWeb\Invoke-WebRequest -websiteSettingList $settings.iis.websites -websiteId "commerceEngine" -relativeUri "commerceops/Bootstrap()" -errorString "*ResponseCode`":`"Error*" -Verbose) -ne 0) { Exit }
 If((ManageWeb\Invoke-WebRequest -websiteSettingList $settings.iis.websites -websiteId "commerceEngine" -relativeUri "commerceops/InitializeEnvironment(environment='HabitatShops')" -errorString "*ResponseCode`":`"Error*" -Verbose) -ne 0) { Exit }
-If((ManageWeb\Invoke-WebRequest -websiteSettingList $settings.iis.websites -websiteId "commerceEngine" -relativeUri "InitializeEnvironment(environment='HabitatAuthoring')" -errorString "*ResponseCode`":`"Error*" -Verbose) -ne 0) { Exit }
-#>
+If((ManageWeb\Invoke-WebRequest -websiteSettingList $settings.iis.websites -websiteId "commerceEngine" -relativeUri "commerceops/InitializeEnvironment(environment='HabitatAuthoring')" -errorString "*ResponseCode`":`"Error*" -Verbose) -ne 0) { Exit }
+
 # Step 12: Creat self signed certificate
-If((ManageIIS\New-Certificate -certificateSettingList $settings.iis.certificates -Verbose) -ne 0) { Exit }
+# If((ManageIIS\New-Certificate -certificateSettingList $settings.iis.certificates -Verbose) -ne 0) { Exit }
