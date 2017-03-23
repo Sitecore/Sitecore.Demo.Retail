d91cf567248799cd313b16f6ebcb4c9e34e406de
J.F. Larente <jflarente@gmail.com>
2017-03-23 11:35:07 -0400

    Adding a TeamCity specific shell to remove the Commerce-related sites for a given instance

---
+++ uninstall-commerce-sites-tc.ps1
@@ -0,0 +1,61 @@
+param([String]$prNumber, [String]$jsonFile)
+Write-Host 'Ready to modify '  $jsonFile 
+
+$catalogDBName = 'habitat'+ $prNumber + '_CommerceServer.ProductCatalog'
+$profileDBName = 'habitat'+ $prNumber + '_CommerceServer.Profiles'
+
+
+Set-ExecutionPolicy Unrestricted –scope CurrentUser
+if (-Not ($PSVersionTable.PSVersion.Major -ge 4)) { Write-Host "Update version of powershell"; exit }
+
+Import-Module $PSScriptRoot\Scripts\Commerce\ManageUser.psm1 -Force
+Import-Module $PSScriptRoot\Scripts\Commerce\ManageCommerceServer.psm1 -Force
+Import-Module $PSScriptRoot\Scripts\Commerce\ManageIIS.psm1 -Force
+Import-Module $PSScriptRoot\Scripts\Commerce\ManageSqlServer.psm1 -Force
+cd $PSScriptRoot
+
+$commerceCatalogDB = 
+
+If ((ManageUser\Confirm-Admin) -ne $true) { Write-Host "Please run script as administrator"; exit }
+
+$settings = ((Get-Content $PSScriptRoot\install-commerce-config.json -Raw) | ConvertFrom-Json)
+
+$installFolderSetting = ($settings.resources | Where { $_.id -eq "install" } | Select)
+If ($installFolderSetting -eq $null) { Write-Host "Expected install resources" -ForegroundColor red; exit; }
+  
+# Step 1: Remove Commerce Server SIte
+Write-Host "`nStep 1: Remove Commerce Server Site" -foregroundcolor Yellow
+if(Get-Module -ListAvailable -Name CSPS) 
+{
+    Import-Module CSPS -Force
+    ManageCommerceServer\Remove-CSWebsite -csSiteSetting $settings.sitecoreCommerce.csSite -Verbose
+}
+Else
+{
+    Write-Host "CSPS PoswerShell Module not installed so assuming no commerce site exists"
+}
+# Step 2: Remove databases from SQL
+Write-Host "`nStep 3: Remove databases from SQL" -foregroundcolor Yellow
+cd SQLSERVER:\SQL
+    
+	ManageSqlServer\Remove-Database -dbname $catalogDBName -Verbose
+	ManageSqlServer\Remove-Database -dbname $profileDBName -Verbose
+
+	cd $PSScriptRoot
+
+# Step 3: Uninstall websites
+Write-Host "`nStep 4: Remove Websites" -foregroundcolor Yellow
+Foreach ($website in $settings.iis.websites)
+{
+    ManageIIS\Remove-Site -name $website.siteName -Verbose
+}
+
+# Step 4: Uninstall Application Pools
+Write-Host "`nStep 5: Remove Application Pools" -foregroundcolor Yellow
+Foreach ($appPool in $settings.iis.appPools)
+{
+    ManageIIS\Remove-AppPool -name $appPool.name -Verbose
+}
+
+
+
